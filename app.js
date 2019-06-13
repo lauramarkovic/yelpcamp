e6de922212c22bec153446aa354d865a17947512
Laura Markovicova <markovicova.laura@gmail.com>
2019-06-13 15:12:04 +0000

    refresh database

--- app.js
+++ app.js
@@ -18,7 +18,15 @@ var campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
 // seedDB(); // seed the database
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v6", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://laurarogers:taminarossa133*@cluster0-6hfgv.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("Connected to DB!");
+}).catch(err => {
+    console.log("ERROR:", err.message);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
